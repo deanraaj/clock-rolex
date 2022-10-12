acdb7cbd02b0e8b5e708bbaa9f6c33f7e9c0eadb
deanraaj <101490408+deanraaj@users.noreply.github.com>
2022-10-12 08:20:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const hourElement = document.querySelector(".hour");
+const minuteElement = document.querySelector(".minute");
+const secondElement = document.querySelector(".second");
+
+function updateClock(){
+    const currentDate =  new Date();
+    //setTimeout(updateClock, 1000);
+    
+
+    const hour = currentDate.getHours();
+    const minute = currentDate.getMinutes();
+    const second = currentDate.getSeconds();
+    
+    const hourDegree = (hour / 12) * 360;
+    hourElement.style.transform = `rotate(${hourDegree}deg)`;
+
+    const minuteDegree = (minute / 60) * 360;
+    minuteElement.style.transform = `rotate(${minuteDegree}deg)`;
+
+    const secondDegree = (second / 60) * 360;
+    secondElement.style.transform = `rotate(${secondDegree}deg)`;
+}
+
+//updateClock();
+
+setInterval(updateClock, 1000);
